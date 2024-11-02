87f9cb22680cd5b4c35ac92dfd8292223fe18e3d
Gleb <w>
2024-11-02 15:41:02 +0300

    Initial Commit

---
+++ Project_Glo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_Glo
+//
+//  Created by Глеб Соколов on 2.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
